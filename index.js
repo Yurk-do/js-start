0d2879ad23261f4d36b0f0782c453b970b418660
Yurk-do <64039182+Yurk-do@users.noreply.github.com>
2020-08-25 23:31:50 +0300

    homework for 21.08.2020

--- index.js
+++ index.js
@@ -1,61 +1,94 @@
 /* eslint-disable no-console */
 
-//Задача № 1. Расстояние между двумя точками в прямоуголной системе координат.
+// Задание 1.1. Заполнить массив 20 числами которые делятся на 3, или на 5, или на 15 и вывести его на экран.
 
-const point1x = -1;
-const point1y = 4;
-
-const point2x = 5;
-const point2y = -2;
-
-const distance = Math.sqrt(
-  Math.pow(point1x - point2x, 2) + Math.pow(point1y - point2y, 2)
-);
-
-console.log(`Расстояние между точками cоставляет: ${distance}`);
-
-//Задача №2. Проверить удовлетворяют ли три числа теоереме Пифагора.
-
-const a = 3;
-const b = 4;
-const c = 6;
-
-const resultYes = a ** 2 + b ** 2 === c ** 2;
-
-let output = " ";
-if (resultYes) {
-  output = "удовлетворяют";
-} else {
-  output = "неудовлетворяют";
+const a = [];
+for (let i = 3; a.length < 20; i += 1) {
+  if (i % 3 === 0 || i % 5 === 0 || i % 15 === 0) {
+    a.push(i);
+  }
 }
 console.log(
-  `Если a = ${a}, b = ${b}, c = ${c}, то три числа ${output} теореме Пифагора`
+  `Массив "a" заполнен 20 числами, которые делятся на 3, или на 5, или на 15: ${a}`
 );
 
-//Задача №3. Убедиться, что три числа могут быть сторонами треугольника.
+// 1.2. Посчитать произведение всех элементов находящихся в каждой третьей позиции массива "a".
 
-const d = 1;
-const e = 3;
-const f = 7;
+let product = 1;
 
-let result = "";
-
-if (d + e > f && e + f > d && f + d > e) {
-  result = "могут";
-} else {
-  result = "не могут";
+for (let i = 2; i < a.length; i += 3) {
+  product *= a[i];
 }
 console.log(
-  `Если d = ${d}, e = ${e}, f = ${f}, то эти три числа ${result} быть сторонами треугольника`
+  `Произведение всех элементов массива "a", находящихся в каждой третьей позиции = ${product}`
 );
 
-//задача №3. Решение с применением тернарного оператора.
-const g = 5;
-const h = 3;
-const i = 7;
+// 1.3. Посчитать среднее арифметическое всех чисел массива "a".
 
-const condition = g + h > i && h + i > g && i + g > h;
-let result2 = condition ? "могут" : "не могут";
+let sum = 0;
+for (let i = 0; i < a.length; i += 1) {
+  sum += a[i];
+}
+const avarage = sum / a.length;
+console.log(`Среднее арифметическое всех чисел массива "a" = ${avarage}`);
+
+// 1.4. Найти максимальное и минимальное число в массиве "а".
+
+let maxNumber = 0;
+
+for (let i = 0; i < a.length; i += 1) {
+  if (a[i] > maxNumber) {
+    maxNumber = a[i];
+  }
+}
+
+let minNumber = maxNumber;
+for (let i = 0; i < a.length; i += 1) {
+  if (a[i] < minNumber) {
+    minNumber = a[i];
+  }
+}
+
 console.log(
-  `Если g = ${g}, h = ${h}, i = ${i}, то эти три числа ${result2} быть сторонами треугольника`
+  `максимальное число массива "a" = ${maxNumber}, минимальное число массива "а" = ${minNumber}`
 );
+
+// 2.1. Вывести матрицу таблицы умножения.
+
+let col1 = [0];
+let col2 = [0];
+let col3 = [0];
+let col4 = [0];
+let col5 = [0];
+let col6 = [0];
+let col7 = [0];
+let col8 = [0];
+let col9 = [0];
+let col10 = [0];
+
+for (let i = 1; i <= 10; i += 1) {
+  col1.push(i);
+  col2.push(col1[i] + i);
+  col3.push(col2[i] + i);
+  col4.push(col3[i] + i);
+  col5.push(col4[i] + i);
+  col6.push(col5[i] + i);
+  col7.push(col6[i] + i);
+  col8.push(col7[i] + i);
+  col9.push(col8[i] + i);
+  col10.push(col9[i] + i);
+  console.log(
+    col1[i],
+    col2[i],
+    col3[i],
+    col4[i],
+    col5[i],
+    col6[i],
+    col7[i],
+    col8[i],
+    col9[i],
+    col10[i]
+  );
+}
+
+// 2.2. Посчитать сумму чисел находящихся на главной диагонали таблицы умножения.
